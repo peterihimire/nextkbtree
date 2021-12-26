83e19002361b7bb04ad2fba6ba285478c8f0cf28
Peter Ihimire <peterihimire@gmail.com>
2021-12-26 21:22:23 +0100

    Working on the home hero

---
+++ components/home/hero/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const HomeHero = () => {
+  return (
+    <section className={styles.hero}>
+      <div className={`${styles.wrapper} wrapper`}>
+        <h1>Crowd Funding Real Estate In Diaspora</h1>
+        <p>
+          Leveraging on tusted Gateway to Financial Freedom. Fast and flexible
+          finance to great, stable returns for investors. . Get started quickly
+          and make your way.
+        </p>
+        <a href="/" className="btn-transparent btn-medium">
+          Shoot Us A Message
+        </a>
+      </div>
+      {/* <div className={styles.buttonGroup}>
+        <button>01</button>
+        <button>02</button>
+        <button>03</button>
+      </div> */}
+    </section>
+  );
+};
+
+export default HomeHero;
