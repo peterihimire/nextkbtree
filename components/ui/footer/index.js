ccf1ee755b9eb82c9fa2d79b094970df03deb95a
Peter Ihimire <peterihimire@gmail.com>
2021-12-28 11:45:49 +0100

    Working on the footer and added the customInput and fontawesome js file for webonline fonts

--- components/ui/footer/index.js
+++ components/ui/footer/index.js
@@ -1,29 +1,60 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import Logo from "../../../public/images/logo-light.svg";
+import Input from "../customInput";
 import ActiveLink from "../../../hoc/activeLink";
 import Link from "next/link";
-
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={`${styles.wrapper} wrapper`}>
         <div className={styles.first}>
-        <div className={styles.logo}>
+          <div className={styles.logo}>
             <Logo />
           </div>
-          <p>
-            32 Heywhy address, 39th Floor Washing, DC 20038
-          </p>
-
+          <p>32 Heywhy address, 39th Floor Washing, DC 20038</p>
         </div>
         <div className={styles.second}>
-
+          <form className={styles.form}>
+            <div className={styles.formGroup}>
+              <Input
+                type="email"
+                id="email"
+                required
+                name="email"
+                placeholder="Join the waiting list (Your email)"
+              />
+            </div>
+          </form>
         </div>
         <div className={styles.last}>
-          
+          <div className={styles.icon}>
+            <img alt="" loading="lazy" src="/images/apple-store-grey.svg" />
           </div>
+          <div className={styles.icon}>
+            <img alt="" loading="lazy" src="/images/google-store-grey.svg" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.outro}>
+        <div className={styles.socials}>
+          <a href="/">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a href="/">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="/">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="/">
+            <i className="fab fa-twitter"></i>
+          </a>
+        </div>
+        <div className={styles.copyright}>
+          <span>© 2021 KoboTree Technologies</span>
+        </div>
       </div>
     </footer>
   );
