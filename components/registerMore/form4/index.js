68a1624dcd47ea09f0680c2bbd6bb7ef3d2735b8
Peter Ihimire <peterihimire@gmail.com>
2022-01-12 15:10:11 +0100

    Rounded up the employment status form with styled radio-buttons

--- components/registerMore/form4/index.js
+++ components/registerMore/form4/index.js
@@ -1,73 +1,87 @@
 import React, { useState } from "react";
 import styles from "./styles.module.scss";
-// import DropXone from "../../ui/customDropzone";
-import Input from "../../ui/customInput";
-import { useDropzone } from "react-dropzone";
 // import Link from "next/link";
 
 const Form = ({ stepHandler, backHandler }) => {
-  const Dropzone = () => {
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
-
-    const files = acceptedFiles.map((file) => (
-      <li key={file.path}>
-        {file.path} - {file.size} bytes
-      </li>
-    ));
-
-    return (
-      <div className="container">
-        <div {...getRootProps({ className: "dropzone" })}>
-          <input {...getInputProps()} />
-          <img src="images/camIcon.svg" alt="" />
-          <p>Upload image here</p>
-          <p>Drag and drop</p>
-        </div>
-        <div className={styles.uploadedFile}>
-          {/* <h4>Files</h4> */}
-          <ul>{files}</ul>
-        </div>
-      </div>
-    );
-  };
-
   return (
     <div className={styles.radioForm}>
-      <button onClick={backHandler} className={styles.backStep}>
-        <img src="/images/arrow-back.svg" alt="" />
-      </button>
+      <div className={styles.backSkipBtn}>
+        <button onClick={backHandler} className={styles.backStep}>
+          <img src="/images/arrow-back.svg" alt="" />
+        </button>
 
-      <h2>Verify your Identity</h2>
+        <button onClick={stepHandler} className={styles.backStep}>
+          Skip
+        </button>
+      </div>
+
+      <h2>Select your Employment Status</h2>
 
       <form>
-        <div className={styles.formGroup}>
+        <div className={styles.radioToolbar}>
           <input
-            className={styles.radioInput}
-            // labelText="Create a Password"
             type="radio"
+            id="radioEmployed"
             name="employmentStatus"
-            id="employed"
-            // checked
-            // required
-            // placeholder="Enter Bank Verification Number"
+            value="employed"
           />
-          <label htmlFor="radioEmployed" className={styles.radioLabel}>
-            Employed
+          <label htmlFor="radioEmployed">
+            <div> Employed</div>
+            <span>
+              <img src="images/check-circle.svg" />
+            </span>
           </label>
-        </div>
 
-        <div className={styles.formGroup}>
           <input
-            className={styles.radioInput}
-            // labelText="Create a Password"
             type="radio"
+            id="radioUnemployed"
             name="employmentStatus"
-            id="unemployed"
-            // required
-            // placeholder="Enter Bank Verification Number"
+            value="unemployed"
           />
-          <label htmlFor="radioUnemployed" className={styles.radioLabel}>
-            Unemployed
+          <label htmlFor="radioUnemployed">
+            <div> Unemployed</div>
+            <span>
+              <img src="images/check-circle.svg" />
+            </span>
+          </label>
+
+          <input
+            type="radio"
+            id="radioSelfemployed"
+            name="employmentStatus"
+            value="selfemployed"
+          />
+          <label htmlFor="radioSelfemployed">
+            <div>Self Employed</div>
+            <span>
+              <img src="images/check-circle.svg" />
+            </span>
+          </label>
+
+          <input
+            type="radio"
+            id="radioRetired"
+            name="employmentStatus"
+            value="retired"
+          />
+          <label htmlFor="radioRetired">
+            <div> Retired</div>
+            <span>
+              <img src="images/check-circle.svg" />
+            </span>
+          </label>
+
+          <input
+            type="radio"
+            id="radioStudent"
+            name="employmentStatus"
+            value="student"
+          />
+          <label htmlFor="radioStudent">
+            <div> Student</div>
+            <span>
+              <img src="images/check-circle.svg" />
+            </span>
           </label>
         </div>
 
