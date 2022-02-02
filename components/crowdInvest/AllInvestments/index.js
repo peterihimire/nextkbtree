f7ce77e8ed96ae831e220f3c83e20c6a62f120d6
Peter Ihimire <peterihimire@gmail.com>
2022-02-02 15:03:28 +0100

    The modal investment is getting in shape, yet to work on the responsiveness and more

--- components/crowdInvest/AllInvestments/index.js
+++ components/crowdInvest/AllInvestments/index.js
@@ -343,8 +343,8 @@ const AllInvestments = () => {
             {tabIndex === 3 && (
               <div className={styles.grid}>
                 <DashboardCard
-                  key='1'
-                  id='1'
+                  key='10'
+                  id='10'
                   src='/images/recommend-invest-img.png'
                   alt='img hot'
                   title='Aklas real estate dual home'
@@ -356,8 +356,8 @@ const AllInvestments = () => {
                   // openModal={applyHandler}
                 />
                 <DashboardCard
-                  key='2'
-                  id='2'
+                  key='23'
+                  id='23'
                   src='/images/recommend-invest-img.png'
                   alt='img'
                   title='Aklas real estate dual home'
@@ -368,8 +368,8 @@ const AllInvestments = () => {
                   raisedAmt='2,000,000'
                 />
                 <DashboardCard
-                  key='3'
-                  id='3'
+                  key='12'
+                  id='12'
                   src='/images/recommend-invest-img.png'
                   alt='img'
                   title='Aklas real estate dual home'
@@ -380,8 +380,8 @@ const AllInvestments = () => {
                   raisedAmt='2,000,000'
                 />
                 <DashboardCard
-                  key='4'
-                  id='4'
+                  key='9'
+                  id='9'
                   src='/images/recommend-invest-img.png'
                   alt='img'
                   title='Aklas real estate dual home'
@@ -392,8 +392,8 @@ const AllInvestments = () => {
                   raisedAmt='2,000,000'
                 />
                 <DashboardCard
-                  key='1'
-                  id='1'
+                  key='15'
+                  id='15'
                   src='/images/recommend-invest-img.png'
                   alt='img hot'
                   title='Aklas real estate dual home'
@@ -405,8 +405,8 @@ const AllInvestments = () => {
                   // openModal={applyHandler}
                 />
                 <DashboardCard
-                  key='2'
-                  id='2'
+                  key='29'
+                  id='29'
                   src='/images/recommend-invest-img.png'
                   alt='img'
                   title='Aklas real estate dual home'
@@ -417,8 +417,8 @@ const AllInvestments = () => {
                   raisedAmt='2,000,000'
                 />
                 <DashboardCard
-                  key='3'
-                  id='3'
+                  key='38'
+                  id='38'
                   src='/images/recommend-invest-img.png'
                   alt='img'
                   title='Aklas real estate dual home'
@@ -429,8 +429,8 @@ const AllInvestments = () => {
                   raisedAmt='2,000,000'
                 />
                 <DashboardCard
-                  key='4'
-                  id='4'
+                  key='48'
+                  id='48'
                   src='/images/recommend-invest-img.png'
                   alt='img'
                   title='Aklas real estate dual home'
@@ -456,135 +456,73 @@ const AllInvestments = () => {
             document.body.classList.remove("_fixed");
           }}
         >
-          <div className={styles.grid}>
-            <div className={styles.left}>
-              <div className={styles.leftWrapper}>
-                <div className={styles.leftContent}>
-                  <img src='/images/investment-details-slide.png' alt='' />
-                  <h3>Property Description</h3>
-                  <p>
-                    This 750+ acre beauty is located in the heart of Lake County
-                    with outstanding views, tree lined acres, grape vineyard and
-                    gorgeous rolling valley hills. This wine country oasis sits
-                    in the Clear Lake AVA and has frontage on State Hwy 53. It
-                    comes with a sophisticated labyrinth of gravel roads that
-                    allows access to the majority of the property and offers a
-                    buyer a tremendous potential to build a premier brand
-                    winery, tasting room, wedding venue, events and agricultural
-                    retail if desired. The property comes with a proven track
-                    record of producing some of the best fruit in Lake County
-                    since it has began its operation in 2006. With over 190
-                    acres of planted vineyards, this property contains 5
-                    separate and distinct varietals of grapes. There are an
-                    additional 166 + acres of plantable land for additional
-                    vineyard to be added to the existing one if wanted. Then you
-                    have over 400 + surplus acres to improve as you wish with
-                    buildings, vineyard support, potential winery, on site
-                    residence and even a tasting room etc. There is a 30
-                    acre-foot reservoir on the property working in conjunction
-                    with six (6) wells. Overhead and underground electric is
-                    developed to portions of the property and to some of the
-                    wells. Private guided tours of the property are available.
-                    Limitless possibilities!
-                  </p>
-
-                  <h3>Location</h3>
-                  <p className={styles.address}>
-                    895 Highway 53, Clearlake, California 95422, USA
-                  </p>
-
-                  <img src='/images/investment-map.png' alt='' />
-
-                  <h4>Optional</h4>
-                  <p>
-                    We can buy real estate property in your company name in the
-                    U.S Corporate Registration (State of Delaware) - We can help
-                    you register a corporation and purchase the Real Estate
-                    property in your corporation name. State of Delaware offers
-                    anonymity, privacy and tax haven for business, it's home to
-                    70% of fortune 500 companies in the world. America is open
-                    to everyone, take advantage today!
-                  </p>
+          <div className={styles.gridWrapper}>
+            <h3>Investment Overview</h3>
+            <div className={styles.grid}>
+              <div className={styles.left}>
+                <div className={styles.leftWrapper}>
+                  <div className={styles.leftContent}>
+                    <img src='/images/investment-details-slide.png' alt='' />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className={styles.right}>
-              <div className={styles.rightWrapper}>
-                <div className={styles.rightContent}>
-                  <div className={styles.rightHeader}>
-                    <div className={styles.headerLeft}>
-                      <p>Property Value</p>
-                      <h3>$150,000,000</h3>
+              <div className={styles.right}>
+                <div className={styles.rightWrapper}>
+                  <div className={styles.rightContent}>
+                    <div className={styles.upDiv}>
+                      <div className={styles.portValue}>
+                        <div>
+                          <p>Portfolio Value</p>
+                          <h3>$9,000,000</h3>
+                        </div>
+                        <div>
+                          <p>Raised</p>
+                          <h3>$2,000,000</h3>
+                        </div>
+                      </div>
+                      <p>Portfolio</p>
+                      <h3>Aklas Real Estate dual Investment</h3>
+                      <p>California USA</p>
                     </div>
-                    <div className={styles.headerRight}>
-                      <span>
-                        <img src='/images/favorite-black.svg' alt='' />
-                      </span>
-                      <span>
-                        <img src='/images/forward-black.svg' alt='' />
-                      </span>
+                    <div className={styles.downDiv}>
+                      <p>Investment ID</p>
+                      <h3>AgrpInvest/KoboTree/1kt980invp</h3>
+                      <div className={styles.unitValue}>
+                        <div>
+                          <p>Unit Value</p>
+                          <h3>$1000</h3>
+                        </div>
+                        <div>
+                          <p>Expected returns</p>
+                          <h3>30% in 9 Months</h3>
+                        </div>
+                      </div>
+                      <p>Payout Type</p>
+                      <h3>Capital + Profit at maturity date</h3>
+
+                      <div className={styles.unitValue}>
+                        <div>
+                          <p>Start Date</p>
+                          <h3>25-Feb-2022</h3>
+                        </div>
+                        <div>
+                          <p>Maturity Date</p>
+                          <h3>25-Nov-2022</h3>
+                        </div>
+                      </div>
+                      <p>Investment Duration</p>
+                      <h3>6 - 9 Months</h3>
                     </div>
                   </div>
-                  <div className={styles.headerAddress}>
-                    <h3>Aklas Real Estate dual Investment</h3>
-                    <p>
-                      217 West 57th Street, 127/128, Central Park Tower, Midtown
-                      ...
-                    </p>
-                  </div>
-
-                  <ul className={styles.investmentOverview}>
-                    <li>
-                      <p>Closing cost:</p>
-                      <span>$150,000,000</span>
-                    </li>
-                    <li>
-                      <p>Deeds transfer:</p>
-                      <span>$1500</span>
-                    </li>
-                    <li>
-                      <p>Mailing of document:</p>
-                      <span>$1,000</span>
-                    </li>
-                    <li>
-                      <p>Temporary fencing:</p>
-                      <span>$5,000</span>
-                    </li>
-                  </ul>
-
-                  <div className={styles.check}>
-                    <label className={`${styles.labelCheck} `}>
-                      <input
-                        id='check'
-                        type='checkbox'
-                        // checked={isChecked}
-                        // onChange={handleOnChange}
-                      />
-                      <span>I want to buy with my company name (optional)</span>
-                    </label>
-
-                    <label className={`${styles.labelCheck} `}>
-                      <input
-                        id='check'
-                        type='checkbox'
-                        // checked={isChecked2}
-                        // onChange={handleOnChange2}
-                      />
-                      <span>
-                        I want to register a new company name (optional)
-                      </span>
-                    </label>
-                  </div>
-
-                  <button className={`btn-medium btn-block ${styles.btnGreen}`}>
-                    Buy this property
-                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <div className={styles.modalFooter} >
 
+          <div className={styles.modalFooter}>
+            <button className={`${styles.invesNowtBtn} btn-primary btn-large`}>
+              Invest Now
+            </button>
           </div>
         </Modal>
       )}
