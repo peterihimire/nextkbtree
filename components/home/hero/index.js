deefd45376b055dc8dd716dc0b916a1e8f4a6320
Peter Ihimire <peterihimire@gmail.com>
2022-03-09 16:35:11 +0100

    Added some new home pictures for the hero slider

--- components/home/hero/index.js
+++ components/home/hero/index.js
@@ -64,97 +64,64 @@ const HomeHero = () => {
         spaceBetween={0}
         slidesPerView={1}
         pagination={pagination}
-        className={`wrapper video ${styles.wrapper}`}
+        className={`${styles.wrapper}`}
         loop
         autoHeight={true}
         centeredSlides={true}
         autoplay={true}
         autoplay={{
-          delay: 13000,
+          delay: 5000,
           disableOnInteraction: false,
         }}
       >
         <SwiperSlide>
-          <div className={styles.slideItem}>
-            <h1>Crowdfunding Real Estate Investment in Diaspora</h1>
-            <p>
-              Leveraging on tusted Gateway to Financial Freedom. Fast and
-              flexible finance to great, stable returns for investors. . Get
-              started quickly and make your way.
-            </p>
-            <a href='/register' className='btn-primary btn-large'>
-              Get Started
-            </a>
+          <div className={styles.slideItem1}>
+            <div className={` wrapper ${styles.wrapper}`}>
+              <h1>Crowdfunding Real Estate Investment in Diaspora</h1>
+              <p>
+                Leveraging on tusted Gateway to Financial Freedom. Fast and
+                flexible finance to great, stable returns for investors. . Get
+                started quickly and make your way.
+              </p>
+              <a href='/register' className='btn-primary btn-large'>
+                Get Started
+              </a>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className={styles.slideItem}>
-            <h1>Crowdfunding Real Estate Investment in Diaspora</h1>
-            <p>
-              Leveraging on tusted Gateway to Financial Freedom. Fast and
-              flexible finance to great, stable returns for investors. . Get
-              started quickly and make your way.
-            </p>
-            <a href='/register' className='btn-primary btn-large'>
-              Get Started
-            </a>
+          <div className={styles.slideItem2}>
+            <div className={` wrapper ${styles.wrapper}`}>
+              <h1>Trowdfunding Real Estate Investment in Diaspora</h1>
+              <p>
+                Leveraging on tusted Gateway to Financial Freedom. Fast and
+                flexible finance to great, stable returns for investors. . Get
+                started quickly and make your way.
+              </p>
+              <a href='/register' className='btn-primary btn-large'>
+                Get Started
+              </a>
+            </div>
           </div>
         </SwiperSlide>
+
         <SwiperSlide>
-          <div className={styles.slideItem}>
-            <h1>Crowdfunding Real Estate Investment in Diaspora</h1>
-            <p>
-              Leveraging on tusted Gateway to Financial Freedom. Fast and
-              flexible finance to great, stable returns for investors. . Get
-              started quickly and make your way.
-            </p>
-            <a href='/register' className='btn-primary btn-large'>
-              Get Started
-            </a>
+          <div className={styles.slideItem3}>
+            <div className={` wrapper ${styles.wrapper}`}>
+              <h1>Trowdfunding Real Estate Investment in Diaspora</h1>
+              <p>
+                Leveraging on tusted Gateway to Financial Freedom. Fast and
+                flexible finance to great, stable returns for investors. . Get
+                started quickly and make your way.
+              </p>
+              <a href='/register' className='btn-primary btn-large'>
+                Get Started
+              </a>
+            </div>
           </div>
         </SwiperSlide>
-      </Swiper>{" "}
+      </Swiper>
       <Accomplishment />
-      {/* <ScrollAnimation
-        animateIn='fadeIn'
-        duration={0.8}
-        delay={400}
-        animateOnce={true}
-      >
-        <div className={` wrapper `}>
-          <div className={styles.accomplishment}>
-            <div className={styles.singleAccomplishment}>
-              <div className={styles.icon}>
-                <img src='/images/investors.svg' alt='' />
-              </div>
-              <div>
-                <h4>150k+</h4>
-                <p>Active Investors</p>
-              </div>
-            </div>
-
-            <div className={styles.singleAccomplishment}>
-              <div className={styles.icon}>
-                <img src='/images/funded.svg' alt='' />
-              </div>
-              <div>
-                <h4>$5.1bn+</h4>
-                <p>Property Funded </p>
-              </div>
-            </div>
-
-            <div className={styles.singleAccomplishment}>
-              <div className={styles.icon}>
-                <img src='/images/earned.svg' alt='' />
-              </div>
-              <div>
-                <h4>$100M+</h4>
-                <p>Earned by Investors</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </ScrollAnimation> */}
     </section>
   );
 };
