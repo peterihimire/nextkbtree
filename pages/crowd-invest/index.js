ce2bd5a659213fdd4af5ced617bfd2096c67c93f
Peter Ihimire <peterihimire@gmail.com>
2022-03-21 23:16:08 +0100

    Removed some redundant un-used commented out code

--- pages/crowd-invest/index.js
+++ pages/crowd-invest/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Layout from "../../layouts/dashboard";
 import AllInvestments from "../../components/crowdInvest/AllInvestments";
-// import DashboardHotInvestment from "../../components/dashboard/hotInvestment";
-// import DashboardRecommendInvestment from "../../components/dashboard/recommendInvestment";
 
 const CrowdInvest = () => {
   return (
     <>
       <AllInvestments />
-      {/* <DashboardRecommendInvestment />
-      <DashboardHotInvestment /> */}
     </>
   );
 };
