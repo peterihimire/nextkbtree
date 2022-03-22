8d4e33d1a80bd9debd9c6cf503c1cca9f8cf2847
Peter Ihimire <peterihimire@gmail.com>
2022-03-22 23:44:03 +0100

    Made sure the kobotree icon on the loginLayout leads back to home

--- components/login/index.js
+++ components/login/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import Form from "./form";
+import Link from "next/link";
 // import Image from "next/image";
-
 
 const Login = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.left}>
-      <div className={styles.inner}>
-          <img src="/images/logo-light.svg" alt="" />
+        <div className={styles.inner}>
+          <Link href='/'>
+            <img src='/images/logo-light.svg' alt='' />
+          </Link>
           <h2>Safe Investments</h2>
           <p>
             Platform that allows you invest securely, safely with guarantee to
             their investment.
           </p>
-          <img src="/images/indicator.svg" alt="" />
+          <img src='/images/indicator.svg' alt='' />
         </div>
       </div>
 
