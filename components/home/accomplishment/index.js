0d42e5bdbe94e215208039fd8bfc2afbf6d8c5dc
Peter Ihimire <peterihimire@gmail.com>
2022-04-17 23:09:27 +0100

    Removed redundant code from accomplishment component

--- components/home/accomplishment/index.js
+++ components/home/accomplishment/index.js
@@ -1,48 +1,40 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import ScrollAnimation from "react-animate-on-scroll";
 
 const Accomplishment = () => {
   return (
     <div className={` wrapper `}>
-      {/* <ScrollAnimation
-        animateIn='fadeIn'
-        duration={0.8}
-        delay={400}
-        animateOnce={true}
-      > */}
-        <div className={styles.accomplishment}>
-          <div className={styles.singleAccomplishment}>
-            <div className={styles.icon}>
-              <img src='/images/investors.svg' alt='' />
-            </div>
-            <div>
-              <h4>150k+</h4>
-              <p>Active Investors</p>
-            </div>
+      <div className={styles.accomplishment}>
+        <div className={styles.singleAccomplishment}>
+          <div className={styles.icon}>
+            <img src='/images/investors.svg' alt='' />
           </div>
-
-          <div className={styles.singleAccomplishment}>
-            <div className={styles.icon}>
-              <img src='/images/funded.svg' alt='' />
-            </div>
-            <div>
-              <h4>$5.1bn+</h4>
-              <p>Property Funded </p>
-            </div>
-          </div>
-
-          <div className={styles.singleAccomplishment}>
-            <div className={styles.icon}>
-              <img src='/images/earned.svg' alt='' />
-            </div>
-            <div>
-              <h4>$100M+</h4>
-              <p>Earned by Investors</p>
-            </div>
+          <div>
+            <h4>150k+</h4>
+            <p>Active Investors</p>
           </div>
         </div>
-      {/* </ScrollAnimation> */}
+
+        <div className={styles.singleAccomplishment}>
+          <div className={styles.icon}>
+            <img src='/images/funded.svg' alt='' />
+          </div>
+          <div>
+            <h4>$5.1bn+</h4>
+            <p>Property Funded </p>
+          </div>
+        </div>
+
+        <div className={styles.singleAccomplishment}>
+          <div className={styles.icon}>
+            <img src='/images/earned.svg' alt='' />
+          </div>
+          <div>
+            <h4>$100M+</h4>
+            <p>Earned by Investors</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
